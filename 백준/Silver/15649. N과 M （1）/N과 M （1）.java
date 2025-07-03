61f71919ba4fdce0df01233485993ecbd9f3ab20
Kantapia0814 <hongseokjin0814@gmail.com>
2025-07-03 12:13:07 +0900

    [Silver III] Title: N과 M (1), Time: 328 ms, Memory: 26116 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] numBox;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        
+        numBox = new int[M];
+        visited = new boolean[N];
+        dfs(0);
+        System.out.println(sb);
+        
+    }
+    static void dfs(int depth) {   
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(numBox[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                numBox[depth] = i + 1;
+                dfs(depth + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
