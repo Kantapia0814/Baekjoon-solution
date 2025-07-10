b4215bdc7d6a9e790843b7256b28c7b9868c1183
Suk Jin Hong <hongseokjin0814@gmail.com>
2025-07-10 14:01:56 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 176 ms, Memory: 17788 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for (int i = 1; i <= N; i++) {
+            String str1 = " ".repeat(N - i) + "*".repeat(i);
+            System.out.println(str1);
+        }
+    }
+}
